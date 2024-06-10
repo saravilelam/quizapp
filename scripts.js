479741e03e6519025fc3245fffd12a211f459650
Sara vmonteiro <sara.bizerra@eaportal.org>
2024-06-10 00:41:07 +0000

    criação de um helper responsavel por mudar o tema

--- scripts.js
+++ scripts.js
@@ -1,39 +1,42 @@
-let tema = "claro"
 
-const botaoTema = document.querySelector(".tema button")
-botaoTema.addEventlistener ("click", trocarTema)
+import {trocarTema, verificarTema} from "./helpers/tema-helper.js"
+
+const botaoTema = document.querySelector(".temabutton")
 
 const body = document.querySelector("body")
 
-function trocarTema () {
-    if (localStorage.getItem("tema")){
+botaoTema.addEventListener("click", () => {
+    trocarTema(body,botaoTema)
+})
 
-        tema = localStorage.setItem("tema")
+export function trocarTema (body, botaoTema) {
+    if (localStorage.getItem("tema")) {
+        tema=localStorage.getItem("tema")
     }
-
     if (tema === "claro") {
-
         body.classList.add("escuro")
-
         localStorage.setItem("tema", "escuro")
-    } 
-    else {
-        body.classList.remove("escuro")
+        botaoTema.style.justifyContent = "flex-end"
 
+    }
+    else{
+        body.classList.remove("escuro")
         localStorage.setItem("tema", "claro")
+        botaoTema.style.justifyContent = "flex-start"
+
     }
 }
 
-function verificarTema() {
+export function  verificarTema(body, botaoTema) {
     if(localStorage.getItem("tema")) {
-        
         tema = localStorage.getItem("tema")
     }
 
     if (tema === "escuro") {
-
         body.classList.add("escuro")
+        botaoTema.style.justifyContent = "flex-end"
     }
 }
 
-verificarTema()
+verificarTema(body, botaoTema)
+    
